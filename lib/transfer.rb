27beeaa057511c0aed4c71ae16d0bf13c5228e1e
Sam D <samidiaz3@gmail.com>
2021-03-24 00:14:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
     #valid?
-    if sender.balance > amount && self.status == "pending" && sender.valid?
+    if sender.balance > amount && self.status == "pending" && valid?
           sender.balance = sender.balance - amount
           receiver.deposit(amount)
           @status = "complete"
