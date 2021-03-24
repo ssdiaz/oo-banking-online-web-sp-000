a3162620cd25fcd15c71cbde15c7a45fed04a52b
Sam D <samidiaz3@gmail.com>
2021-03-24 00:03:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,9 +21,9 @@ class Transfer
           sender.balance = sender.balance - amount
           receiver.deposit(amount)
           @status = "complete"
-      else
+    else
         print "Transaction rejected. Please check your account balance."
-      end
+    end
 
   end
 
