e9bca7e1fe66c2ba93d7a43b481a5e476f53641c
Sam D <samidiaz3@gmail.com>
2021-03-24 00:18:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,8 @@ class Transfer
           receiver.balance += amount
           self.status = "complete"
     else
+          self.status = "rejected"
          "Transaction rejected. Please check your account balance."
-         self.status = "rejected"
     end
 
   end
